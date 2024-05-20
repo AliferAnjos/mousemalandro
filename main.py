27264fb7d240f989db3783d35623b7a74fc890f8
AliferAnjos <156974184+AliferAnjos@users.noreply.github.com>
2024-05-20 17:01:01 -0300

    Update main.py
    Debuggin

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 screen = Tk()
 screen.title("Mouse Malandro")
 screen.geometry("400x400")
-screen.config(bg="#512800")
+screen.config(bg="#222222")
 screen.resizable(False, False)
 
 
@@ -15,43 +15,48 @@ logo = PhotoImage(width=350, height=350, file="mickey.png")
 foto = Label(image=logo)
 foto.place(x=23, y=25)
 
+
 global RUNNING
 RUNNING = True
 
 
 def start():
-        play = Button(bg="#0e1b29", fg="white", text="RUNNING", height=5, width=10, font="Consolas")
-        play.place(x=50, y=250)
+    global RUNNING
+    RUNNING = True
+    play = Button(bg="#0e1b29", fg="white", text="RUNNING", height=5, width=10, font="Consolas")
+    play.place(x=50, y=250)
+    screen.update()
+                    
+    while RUNNING:
         screen.update()
-                
-        while RUNNING:
-            screen.update()
-            xcor = random.randint(200, 800)
-            ycor = random.randint(200, 600)
-            time.sleep(5)
-            pg.moveTo(xcor, ycor)
-
-
-play = Button(bg="#556b2f", fg="white", text="START", height=5, width=10, font="Consolas", command=start)
-play.place(x=50, y=250)
-
+        xcor = random.randint(200, 800)
+        ycor = random.randint(200, 600)
+        time.sleep(5)
+        pg.moveTo(xcor, ycor)
+        if RUNNING == False:
+            return
+            
 
 def stop():
     global RUNNING
     RUNNING = False
+
     play = Button(bg="#556b2f", fg="white", text="START", height=5, width=10, font="Consolas", command=start)
     play.place(x=50, y=250)
-    stop = Button(bg="#0e1b29", fg="white", text="STOP", height=5, width=10, font="Consolas")
-    stop.place(x=250, y=250)
+    kill = Button(bg="#0e1b29", fg="white", text="STOP", height=5, width=10, font="Consolas")
+    kill.place(x=250, y=250)
     screen.update()
     time.sleep(1)
     screen.update()
-    stop = Button(bg="#800000", fg="white", text="STOP", height=5, width=10, font="Consolas", command=stop)
-    stop.place(x=250, y=250)
+    kill = Button(bg="#800000", fg="white", text="STOP", height=5, width=10, font="Consolas", command=stop)
+    kill.place(x=250, y=250)
+    return
 
+play = Button(bg="#556b2f", fg="white", text="START", height=5, width=10, font="Consolas", command=start)
+play.place(x=50, y=250)
 
-stop = Button(bg="#800000", fg="white", text="STOP", height=5, width=10, font="Consolas", command=stop)
-stop.place(x=250, y=250)
+kill = Button(bg="#800000", fg="white", text="STOP", height=5, width=10, font="Consolas", command=stop)
+kill.place(x=250, y=250)
 
 
 screen.mainloop()
